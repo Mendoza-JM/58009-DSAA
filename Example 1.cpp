aa0d716d4a2cdb13ee8aa9d6e5755349ee9b259e
Mendoza-JM <123426445+Mendoza-JM@users.noreply.github.com>
2023-08-18 12:02:17 +0800

    Create Example 1.cpp

---
+++ Example 1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int x, y, z;
+    cout<< "Solve for the area of a Rectangle" << endl;
+    cout<<"Please enter the length = ";
+    cin >> x;
+    cout<<"Please enter the width = ";
+    cin >> y;
+    z=x*y;
+    cout<<"\n\n The area of your Rectangle is "<<z;
+    cout<<" sq. units";
+
+    return 0;
+}
